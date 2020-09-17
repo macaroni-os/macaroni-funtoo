32dc40eea6e2c426a09db3a90afc1c43598fe21b
Daniele Rondina <geaaru@sabayonlinux.org>
2020-09-17 17:55:07 +0200

    distro/funtoo-stage3: Bump v.1.4+2020.09.07.1

--- amd64/packages/funtoo-seed/1.4/build.sh
+++ amd64/packages/funtoo-seed/1.4/build.sh
@@ -4,10 +4,11 @@ apk --no-cache add ca-certificates tar wget xz rsync
 
 FUNTOO_RELEASE="1.4"
 DATE_VERSION=$(echo "${PACKAGE_VERSION}" | cut -d'+' -f 2)
+DATE_VERSION=${DATE_VERSION:0:10}
 DATE_VERSION=${DATE_VERSION//./-}
 
 FUNTOO_URL=https://build.funtoo.org/${FUNTOO_RELEASE}-release-std
-FUNTOO_ARCH=x86-64
+FUNTOO_ARCH=x86-64bit
 FUNTOO_SUBARCH=generic_64
 
 wget ${FUNTOO_URL}/${FUNTOO_ARCH}/${FUNTOO_SUBARCH}/${DATE_VERSION}/stage3-${FUNTOO_SUBARCH}-${FUNTOO_RELEASE}-release-std-${DATE_VERSION}.tar.xz -O /rootfs.tar.xz
