402bf8e3fb69df8d8f5b4c7341d675c5a6916715
Daniele Rondina <geaaru@sabayonlinux.org>
2020-09-21 23:31:30 +0200

    distro/funtoo-stage3: Review build.sh for docker build

--- amd64/packages/funtoo-seed/1.4/build.sh
+++ amd64/packages/funtoo-seed/1.4/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -ex
-apk --no-cache add ca-certificates tar wget xz rsync
-
 FUNTOO_RELEASE="1.4"
 DATE_VERSION=$(echo "${PACKAGE_VERSION}" | cut -d'+' -f 2)
 DATE_VERSION=${DATE_VERSION:0:10}
@@ -11,7 +9,4 @@ FUNTOO_URL=https://build.funtoo.org/${FUNTOO_RELEASE}-release-std
 FUNTOO_ARCH=x86-64bit
 FUNTOO_SUBARCH=generic_64
 
-wget ${FUNTOO_URL}/${FUNTOO_ARCH}/${FUNTOO_SUBARCH}/${DATE_VERSION}/stage3-${FUNTOO_SUBARCH}-${FUNTOO_RELEASE}-release-std-${DATE_VERSION}.tar.xz -O /rootfs.tar.xz
-
-mkdir /temp
-cd /temp && tar -xJf /rootfs.tar.xz --xattrs --numeric-owner && rm /rootfs.tar.xz
+wget ${FUNTOO_URL}/${FUNTOO_ARCH}/${FUNTOO_SUBARCH}/${DATE_VERSION}/stage3-${FUNTOO_SUBARCH}-${FUNTOO_RELEASE}-release-std-${DATE_VERSION}.tar.xz -O rootfs.tar.xz
