d8a756daf00259ec36d76ce08fed6a1622c7bc22
Daniele Rondina <geaaru@gmail.com>
2022-05-20 08:36:09 +0200

    macaroni/live-setup: Bump v.0.14

--- packages/macaroni/live-setup/live-functions.sh
+++ packages/macaroni/live-setup/live-functions.sh
@@ -383,7 +383,7 @@ prepare() {
   fi
 
   # Temporary fix until entities will handle this
-  mkdir -p /home/macaroni
+  cp -rvf /etc/skel/ /home/macaroni/
   chown macaroni:users -R /home/macaroni
 
   #sed -i -e 's|INACTIVE_TIMEOUT.*|INACTIVE_TIMEOUT=60|g' /etc/conf.d/NetworkManager
