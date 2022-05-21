052390e420b067a0547c64c6390a3a28ddc42879
Daniele Rondina <geaaru@gmail.com>
2022-05-21 08:12:42 +0200

    macaroni/live-setup: Bump v.0.14+2

--- packages/macaroni/live-setup/live-functions.sh
+++ packages/macaroni/live-setup/live-functions.sh
@@ -392,8 +392,10 @@ prepare() {
   echo "macaroni ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/99-macaroni
 
   dash2dockid=$(eselect gnome-shell-extensions list  | grep dash-to-dock | awk '{ print $1 }' | sed -e 's|\[||g' -e 's|\]||g')
+  desktopiconsid=$(eselect gnome-shell-extensions list  | grep desktop-icons| awk '{ print $1 }' | sed -e 's|\[||g' -e 's|\]||g')
   eselect gnome-shell-extensions list
   eselect gnome-shell-extensions enable ${dash2dockid}
+  eselect gnome-shell-extensions enable ${desktopiconsid}
   eselect gnome-shell-extensions list
 
   ldconfig
