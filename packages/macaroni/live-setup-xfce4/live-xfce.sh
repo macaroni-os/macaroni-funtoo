25b97cb49997400169e5ec56b71322f41d630518
Daniele Rondina <geaaru@gmail.com>
2022-10-06 08:03:26 +0200

    macaroni/live-setup-xfce4: Bump v.0.1

---
+++ packages/macaroni/live-setup-xfce4/live-xfce.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [ -n "$DEBUG" ] ; then
+  set -x
+fi
+
+EROOT=${EROOT:-""}
+ENABLED_BOOT_SERVICES=(
+  "dbus"
+  "binfmt"
+  "elogind"
+  "hostname"
+  "opentmpfiles-setup"
+  "procfs"
+  "root"
+  "swap"
+  "urandom"
+)
+ENABLED_SERVICES=(
+  "avahi-daemon"
+  "local"
+  "bluetooth"
+  # Temporay enable logger always. On ISO probably we can to maintain
+  # this off.
+  "metalog"
+  "NetworkManager"
+  "xdm"
+  "virtualbox-guest-additions"
+)
+ENABLED_SYSINIT_SERVICES=(
+  "udev-postmount"
+  "udev-trigger"
+  "udev-settle"
+  "cgroups"
+  "devfs"
+  # dmcrypt and device-mapper is needed to support encrypted filesystem
+  "device-mapper"
+  "dmcrypt"
+  "dmesg"
+  "sysfs"
+)
+HOOKS=(
+  "vboxguest.vboxguest_setup"
+  "fonts.convert_pfb"
+  "fonts.setup_all_fonts"
+  "gtk.glib_update_schemas"
+  "gtk.gtk_update_icons"
+  "gtk.mime_update_db"
+)
+
+source ${EROOT}/var/lib/macaroni/live-core.sh
+
+main_xfce() {
+  main || return 1
+}
+
+main_xfce
+exit $?
