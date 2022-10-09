7c8c2390a4e88d9b9a04d0155366fd2956e81e39
Daniele Rondina <geaaru@gmail.com>
2022-10-09 09:05:45 +0200

    macaroni/live-setup-core: Bump v.0.2

--- packages/macaroni/live-setup-core/scripts.d/10-xorg.sh
+++ packages/macaroni/live-setup-core/scripts.d/10-xorg.sh
@@ -4,4 +4,17 @@ setup_xorg() {
   mkdir -p /etc/X11/ || true
 
   eselect opengl set xorg-x11 --use-old
+
+  setup_default_xsession
 }
+
+setup_default_xsession() {
+  if [ -n "${DEFAULT_XSESSION}" ] ; then
+    local sess="${DEFAULT_XSESSION}"
+    echo "[Desktop]" > /etc/skel/.dmrc
+    echo "Session=${sess}" >> /etc/skel/.dmrc
+    rm -vf /usr/share/xsessions/default.desktop || true
+    ln -sf "${sess}.desktop" /usr/share/xsessions/default.desktop
+  fi
+}
+
