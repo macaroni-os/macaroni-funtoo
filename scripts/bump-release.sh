18dd707c967138df45a4d271ca2dac938ceb9830
Daniele Rondina <geaaru@gmail.com>
2023-03-17 18:40:34 +0100

    portage-converter: Update specs for lsb-release, baselayout

--- scripts/bump-release.sh
+++ scripts/bump-release.sh
@@ -4,8 +4,8 @@
 # NOTE: this script MUST be execute at the repo root level.
 
 files2bump=(
-  "packages/atoms/base/sys-apps/baselayout/definition.yaml"
-  "packages/atoms/base/sys-apps/lsb-release/definition.yaml"
+  "packages/atoms/release/sys-apps/baselayout/definition.yaml"
+  "packages/atoms/release/sys-apps/lsb-release/definition.yaml"
 )
 
 
