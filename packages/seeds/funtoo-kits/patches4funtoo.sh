339c5b591a1d80077768d1397f48136ab2fbdddb
Daniele Rondina <geaaru@gmail.com>
2023-08-28 09:55:48 +0200

    seed/funtoo-kits: Fix typo

--- packages/seeds/funtoo-kits/patches4funtoo.sh
+++ packages/seeds/funtoo-kits/patches4funtoo.sh
@@ -3,7 +3,7 @@
 # Macaroni OS patches for Funtoo
 
 # Fix zziplib to use python3+
-rm -rf /var/git/meta-repo/kits/dev-kit/zziplib/
-cp -rvf patches/zziplib/ /var/git/meta-repo/kits/dev-kit/
+rm -rf /var/git/meta-repo/kits/dev-kit/dev-libs/zziplib/
+cp -rvf patches/zziplib/ /var/git/meta-repo/kits/dev-kit/dev-libs/
 
 
