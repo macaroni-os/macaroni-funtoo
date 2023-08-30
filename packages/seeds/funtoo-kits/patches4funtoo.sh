75a727311ebe0409654d6d0efc5da6a2ea307fd6
Daniele Rondina <geaaru@gmail.com>
2023-08-30 22:36:16 +0200

    seed/funtoo-kits: Bump v.0.20230805+7

--- packages/seeds/funtoo-kits/patches4funtoo.sh
+++ packages/seeds/funtoo-kits/patches4funtoo.sh
@@ -6,4 +6,6 @@
 rm -rf /var/git/meta-repo/kits/dev-kit/dev-libs/zziplib/
 cp -rvf patches/zziplib/ /var/git/meta-repo/kits/dev-kit/dev-libs/
 
-
+# Permits compilation of qt-webkit with ruby3.1
+sed -i -e 's|^USE_RUBY=.*|USE_RUBY="ruby25 ruby26 ruby27 ruby31"|g' \
+  /var/git/meta-repo/kits/qt-kit/dev-qt/qtwebkit/qtwebkit-*.ebuild
