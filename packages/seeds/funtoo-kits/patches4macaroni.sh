27a91fff3ae585a31757df6e25b8aa8942513f41
Daniele Rondina <geaaru@gmail.com>
2023-09-02 20:20:35 +0200

    seed/funtoo-kits: Fix typo

--- packages/seeds/funtoo-kits/patches4macaroni.sh
+++ packages/seeds/funtoo-kits/patches4macaroni.sh
@@ -6,7 +6,7 @@
 # In the near future I want to follow the old Sabayon way
 # about the pinentry package to have pinentry-qt, pinentry-gnome, pinentry-gtk.
 sed -i -e 's|^PDEPEND.*|PDEPEND="app-crypt/pinentry"|g' \
-  gnome-base/gnome-keyring/gnome-keyring-*.ebuild
+  /var/git/meta-repo/kits/gnome-kit/gnome-base/gnome-keyring/gnome-keyring-*.ebuild
 
 # Drop package.mask with elogind use flag for x11-misc/sddm
 rm /var/git/meta-repo/kits/core-kit/profiles/funtoo/1.0/linux-gnu/flavor/workstation/package.use.mask
