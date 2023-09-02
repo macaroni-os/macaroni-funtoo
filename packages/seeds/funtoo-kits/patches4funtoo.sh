1574c40e5455d9306ae2cec3da12155b0ce99dd4
Daniele Rondina <geaaru@gmail.com>
2023-09-02 18:15:41 +0200

    seed/funtoo-kits: Bump v.0.20230805+8

--- packages/seeds/funtoo-kits/patches4funtoo.sh
+++ packages/seeds/funtoo-kits/patches4funtoo.sh
@@ -9,3 +9,25 @@ cp -rvf patches/zziplib/ /var/git/meta-repo/kits/dev-kit/dev-libs/
 # Permits compilation of qt-webkit with ruby3.1
 sed -i -e 's|^USE_RUBY=.*|USE_RUBY="ruby25 ruby26 ruby27 ruby31"|g' \
   /var/git/meta-repo/kits/qt-kit/dev-qt/qtwebkit/qtwebkit-*.ebuild
+
+# ibus upgraded and available on geaaru-kit. TODO: open a PR to Funtoo
+
+# Fix wrong DEPEND on source-hightlight
+sed -i -e 's|>=dev-libs/boost-1.62.0\:=\[threads\]|dev-libs/boost|g' \
+  /var/git/meta-repo/kits/dev-kit/dev-util/source-highlight/source-highlight-*.ebuild
+
+# Fix typo on x11-base/xorg-drivers ebuild
+sed -i -e 's|xf86-video-vboxvideo|xf86-video-vbox|g' \
+  /var/git/meta-repo/kits/core-gl-kit/x11-base/xorg-drivers/xorg-drivers-*.ebuild
+
+# Permit using vala 0.54
+sed -i -e 's|0.50|0.54|g' \
+  /var/git/meta-repo/kits/core-kit/eclass/vala.eclass
+
+# Drop pam/elogind dependencies cross referencies
+sed -i -e 's|elogind? ( .* )||g' -e 's|elogind||g' \
+  /var/git/meta-repo/kits/core-kit/sys-libs/pam/pam-*.ebuild
+
+sed -i -e '/sys-libs\/pam\[elogind\]/d' \
+  /var/git/meta-repo/kits/core-gl-kit/x11-base/xorg-server/xorg-server-*.ebuild
+
