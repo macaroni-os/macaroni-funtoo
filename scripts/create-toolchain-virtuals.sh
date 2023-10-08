7a66bdb69d3df47a89e2eb54c5ee5f8261e7dac6
Daniele Rondina <geaaru@gmail.com>
2023-10-08 22:10:05 +0200

    scripts/create-toolchain-virtuals.sh: Generate local idx

--- scripts/create-toolchain-virtuals.sh
+++ scripts/create-toolchain-virtuals.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+LUET_BUILD=${LUET_BUILD:-luet-build}
 
 kits=(
   "base"
@@ -11,5 +12,13 @@ kits=(
 )
 
 for i in ${kits[@]} ; do
+  # Generate the index of the directory
+  ${LUET_BUILD} tree genidx --only-upper-level --compress=false -t packages/atoms/${i} || {
+    echo "Error on generate indexes for ${i}"
+    exit 1
+  }
+
   TARGET_PKG=packages/toolchain/${i} SOURCE_PKGS_DIR=packages/atoms/${i}  ${SCRIPT_DIR}/toolchain-requires.sh
+
+  rm packages/atoms/${i}/.anise-idx.json
 done
