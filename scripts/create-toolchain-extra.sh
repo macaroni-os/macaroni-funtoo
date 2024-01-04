10f8375381e01eb42a60a401db80f688dca7ca91
Daniele Rondina <geaaru@gmail.com>
2024-01-04 19:48:02 +0100

    scripts/create-toolchain-extra.sh: Using genidx

--- scripts/create-toolchain-extra.sh
+++ scripts/create-toolchain-extra.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+LUET_BUILD=${LUET_BUILD:-luet-build}
 
 kits=(
   "net"
@@ -15,5 +16,12 @@ kits=(
 )
 
 for i in ${kits[@]} ; do
+  # Generate the index of the directory
+  ${LUET_BUILD} tree genidx --only-upper-level --compress=false -t packages/atoms-extra/${i} || {
+    echo "Error on generate indexes for ${i}"
+    exit 1
+  }
   TARGET_PKG=packages/toolchain/extra-${i} SOURCE_PKGS_DIR=packages/atoms-extra/${i}  ${SCRIPT_DIR}/toolchain-requires.sh
+
+  rm packages/atoms-extra/${i}/.anise-idx.json
 done
