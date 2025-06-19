d1bc47ac20fb167259eb85b6278b1ffb7cd4693d
Daniele Rondina <geaaru@macaronios.org>
2025-06-19 09:30:57 +0200

    macaroni/live-setup-core: Bump v.0.9.0

--- packages/macaroni/live-setup-core/build-iso-hook.sh
+++ packages/macaroni/live-setup-core/build-iso-hook.sh
@@ -4,11 +4,13 @@
 set -x
 set -e
 
-setup_luet_config() {
-  yq w /etc/luet/luet.yaml 'general.debug' "false" -i
-  yq d /etc/luet/luet.yaml 'finalizer_envs' -i
-  yq w /etc/luet/luet.yaml 'general.spinner_charset' "39" -i
-  yq d /etc/luet/luet.yaml 'general.spinner_ms' -i
+yqbin=/usr/bin/yq4
+
+setup_anise_config() {
+  ${yqbin} '.general.debug = false' /etc/luet/luet.yaml -i
+  ${yqbin} '.general.spinner_charset = 39' /etc/luet/luet.yaml -i
+  ${yqbin} 'del(.finalizer_envs)' /etc/luet/luet.yaml -i
+  ${yqbin} 'del(.general.spinner_ms)' /etc/luet/luet.yaml -i
 
   # Setup config protect option.
   mkdir -p /etc/luet/config.protect.d || true
